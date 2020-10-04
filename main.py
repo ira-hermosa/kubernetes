b73c369d466686be7045c19f5388d39219d192ef
ira-hermosa <54219989+ira-hermosa@users.noreply.github.com>
2020-10-04 22:33:33 +0100

    revert commit 2df0c50f36
    https://knowledge.udacity.com/questions/185757

--- main.py
+++ main.py
@@ -12,13 +12,7 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 
-#JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
-import boto3
-ssm = boto3.client("ssm")
-JWT_SECRET = ssm.get_parameter(
-    Name='JWT_SECRET',
-    WithDecryption=True
-) 
+JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
